25b7bde55c2e371b7aa48be0878be6c803ef65cf
Alexey Badalov <alexey.badalov@cern.ch>
2013-01-13 08:06:28 +0100

    Slight refactored daemonization.

--- FastTrackD/src/main.cpp
+++ FastTrackD/src/main.cpp
@@ -4,49 +4,40 @@
 
 #include <cstdlib>
 #include <exception>
-#include <iostream>
-#include <string>
+#include <stdexcept>
 
 using namespace std;
 
-// main entry point
-// creates a daemon, starts logging, and handles uncaught exceptions
-int main(int argc, char * argv[])
-try
+void daemonize()
 {
-  // fork off the partent process
+  // fork off the parent process
   pid_t pid(fork());
   if (pid > 0)
-  {
-    return EXIT_SUCCESS;
-  }
+    exit(EXIT_SUCCESS);
   if (pid < 0)
-  {
-    cerr << "Fork failed." << endl;
-    return EXIT_FAILURE;
-  }
+    throw runtime_error("Fork failed.");
 
   // change the file mode mask
   umask(0);
 
   // create a new SID for the child process
-  pid_t sid(setsid());
-  if (sid < 0)
-  {
-    cerr << "Failed to create a new child process SID." << endl;
-    return EXIT_FAILURE;
-  }
+  if (setsid() < 0)
+    throw runtime_error("Failed to create a new child process SID.");
 
   // change the current working directory
   if (chdir("/") < 0)
-  {
-    cerr << "Failed to change the current working directory." << endl;
-    return EXIT_FAILURE;
-  }
+    throw runtime_error("Failed to change the current working directory.");
+}
 
-  fclose (stdin);
-  fclose (stdout);
-  fclose (stderr);
+// main entry point
+int main(int argc, char * argv[])
+try
+{
+  daemonize();
+
+  freopen("/dev/null", "r", stdin);
+  freopen("/dev/null", "w", stdout);
+  freopen("/dev/null", "w", stderr);
 
   openlog("fasttrackd", LOG_CONS, LOG_DAEMON);
 
@@ -59,8 +50,10 @@ try
 catch (const exception & e)
 {
   syslog(LOG_ERR, "Unrecoverable error: %s", e.what());
+  return EXIT_FAILURE;
 }
 catch (...)
 {
   syslog(LOG_ERR, "Unknown unrecoverable error.");
+  return EXIT_FAILURE;
 }
