c6c7d95a1a74033d61dc71efe38c539b0c26d328
Alexey Badalov <alexey.badalov@cern.ch>
2014-12-12 07:52:59 +0100

    GpuIpc bugfix.

--- Brunel_v47r2p1/GpuManager/GpuIpc/src/LocalSocketClient.cpp
+++ Brunel_v47r2p1/GpuManager/GpuIpc/src/LocalSocketClient.cpp
@@ -43,7 +43,7 @@ LocalSocketClient::~LocalSocketClient() {
 void LocalSocketClient::readBytes(void * data, size_t size) {
   size_t total = 0u;
   while (total < size) {
-    size_t received = read(m_socket, data, size - total);
+    size_t received = read(m_socket, static_cast<uint8_t*>(data) + total, size - total);
     if (received == static_cast<size_t>(-1)) {
       std::stringstream msg;
       msg << "Read error; " << total << " bytes received.";
